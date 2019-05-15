4f9c8bdbe18b651b94b78d44cf01387a15153822
UID <UID@DESKTOP-DACJOCN>
2019-05-15 10:20:29 +0800

    test

---
+++ src/com/jp/service/CartItemService.java
@@ -0,0 +1,21 @@
+package com.jp.service;
+
+import java.util.List;
+
+import com.jp.po.CartDetails;
+import com.jp.po.Cartitem;
+
+public interface CartItemService {
+	
+	public List<CartDetails> getCartItemByUserId(Integer userid) throws Exception;
+	
+	public List<CartDetails> getCartItemByCartitemid(String[] cartitemid) throws Exception;
+	
+	public int addToCart(String lsbn,int userid,int num) throws Exception;
+	
+	public void deleteCartitemByCartitemid(Integer cartitemid) throws Exception;
+	
+	public void batchDeleteCartitem(String cartitemids) throws Exception;
+	
+	public boolean updateQuantity(Integer cartitemid,Integer num) throws Exception;
+}
